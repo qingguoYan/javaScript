4819a93137168d25dbc4ef296318f0a14e73e3b2
qingguoYan <863235011@qq.com>
2021-05-23 21:53:45 +0800

    react

--- project/video/app.js
+++ project/video/app.js
@@ -1,20 +1,20 @@
-const video = document.querySelector(".video");
-const btn = document.querySelector(".switch-btn");
+const video = document.querySelector('.video');
+const btn = document.querySelector('.switch-btn');
 
-btn.addEventListener("click", function () {
+btn.addEventListener('click', function () {
   console.log(btn.classList);
-  if (!btn.classList.contains("slide")) {
-    btn.classList.add("slide");
+  if (!btn.classList.contains('slide')) {
+    btn.classList.add('slide');
     video.pause();
   } else {
-    btn.classList.remove("slide");
+    btn.classList.remove('slide');
     video.play();
   }
 });
 
-const preloader = document.querySelector(".preloader");
+const preloader = document.querySelector('.preloader');
 //load 事件会在所有资源加载完毕后执行，DOMContentLoaded事件会在dom元素加载完毕后执行
-window.addEventListener("load", function () {
-  console.log("加载完毕");
-  preloader.classList.add("hide");
+window.addEventListener('load', function () {
+  console.log('加载完毕');
+  preloader.classList.add('hide');
 });
